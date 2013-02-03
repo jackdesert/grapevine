b99c0b82771817e193f2a8a143bc1d44fad5dcdf
Jack Desert <jackdesert@gmail.com>
2013-02-02 17:12:48 -0800

    initial commit Rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grapevine::Application.config.secret_token = '82d2493dcf4cbce4718bf6796f2c1348d9fe979ea5f8df57cd5b4c783370041a7b844cd1e53eb38b7bb1d6dd2e2b1ba73ce659a3bb2b78d772b197fe4c9a49d9'
